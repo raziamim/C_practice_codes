1d6cad687d6c5523d9826d312d4f294cbff83033
Razia Mim <raziamim10@gmail.com>
2024-03-03 19:43:17 +0600

    added

---
+++ productInteger/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int firstInteger,secondInteger;
+    printf("The first integer is:");
+    scanf("%d",&firstInteger);
+    printf("The second integer is:");
+    scanf("%d",&secondInteger);
+    printf("Product of the above two integer is:%d",firstInteger*secondInteger);
+
+    return 0;
+}
