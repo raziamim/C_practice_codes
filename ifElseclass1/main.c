ff618456a2f6522bfdefdcf39c67399ce41ecbe8
Razia Mim <raziamim10@gmail.com>
2024-03-15 15:49:24 +0600

    added

---
+++ ifElseclass1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x=15;
+    int y=15;
+    if (x<=y){
+            printf("X is less/equal than Y");
+
+    }
+    else{
+        printf("X is greater than Y");
+    }
+    return 0;
+}
