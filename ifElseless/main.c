38cc53027e43a31e13da415f0389ba095efa7ade
Razia Mim <raziamim10@gmail.com>
2024-03-15 16:02:35 +0600

    added

---
+++ ifElseless/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x=10;
+    int y=7;
+    if (x<y){
+        printf("X is less than Y");
+    }
+    else{
+        printf("X is greater than Y");
+    }
+
+
+    return 0;
+}
