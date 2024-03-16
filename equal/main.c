c9b0ad89725478681a4ba942a7de1d62b307ed0e
Razia Mim <raziamim10@gmail.com>
2024-03-16 14:17:47 +0600

    added"

---
+++ equal/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 6;
+    int y = 5;
+    if (x==y){
+        printf("The two variables are equal");
+    }
+    else{
+        printf("The variables are not equal");
+    }
+
+    return 0;
+}
